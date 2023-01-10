c313a6810b9bc518aa1168691dd8cba3df7cb2b7
hassanbaryal <h.baryal2@gmail.com>
2023-01-10 18:44:47 -0500

    Added level select route

--- src/components/Routes/RouteSwitch.jsx
+++ src/components/Routes/RouteSwitch.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import Home from '../Home/Home';
+import LevelSelect from '../LevelSelect/LevelSelect';
 
 function RouteSwitch() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/levelselect" element={<LevelSelect />} />
       </Routes>
     </BrowserRouter>
   );
