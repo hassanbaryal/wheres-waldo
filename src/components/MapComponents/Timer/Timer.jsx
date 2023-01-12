2bfea6d5f8bca765143ac43a73436513f762dbe4
hassanbaryal <h.baryal2@gmail.com>
2023-01-11 20:02:52 -0500

    Created Timer component

---
+++ src/components/MapComponents/Timer/Timer.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+
+function Timer({ counter, setCounter, counterStop }) {
+  useEffect(() => {
+    const interval = setInterval(() => setCounter(counter + 1), 1000);
+
+    if (counterStop) clearInterval(interval);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [counter, counterStop]);
+
+  return (
+    <p className="text-6xl text-center font-bold">
+      Time Elapsed:
+      {counter}
+      s
+    </p>
+  );
+}
+
+export default Timer;
