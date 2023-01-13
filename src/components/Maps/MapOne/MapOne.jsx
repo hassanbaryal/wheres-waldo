def81995cb4d9849a477ca5d17161ce6f95b7092
hassanbaryal <h.baryal2@gmail.com>
2023-01-13 18:04:28 -0500

    Added temporary map object

--- src/components/Maps/MapOne/MapOne.jsx
+++ src/components/Maps/MapOne/MapOne.jsx
@@ -6,11 +6,30 @@ import TargetSelectorPopUp from '../../MapComponents/TargetSelectorPopUp/TargetS
 import Targets from '../../MapComponents/Targets/Targets';
 
 function MapOne() {
+  // For timer
   const [counter, setCounter] = useState(0);
   const [counterStop, setCounterStop] = useState(false);
+  // For popup when clicking on map
   const [popUpVisible, setPopUpVisible] = useState(false);
   const [xCoord, setXCoord] = useState(0);
   const [yCoord, setYCoord] = useState(0);
+  // Variable to store map data (fetch it). Temporarily filled
+  const [map, setMap] = useState({
+    characters: [
+      {
+        name: 'Homer Simpson (The Simpsons)',
+        location: null,
+      },
+      {
+        name: 'Aang (ATLA)',
+        location: null,
+      },
+      {
+        name: 'Stewie (Family guy)',
+        location: null,
+      },
+    ],
+  });
 
   const handleClick = (e) => {
     if (!popUpVisible) {
@@ -42,13 +61,22 @@ function MapOne() {
         />
         <div className="flex justify-center items-center gap-6">
           <p>Tap for Targets:</p>
-          <Targets />
+          <Targets
+            characters={map.characters}
+          />
         </div>
       </header>
       <div className="relative">
         <img src={mapOneImg} alt="" />
         {
-          popUpVisible && <TargetSelectorPopUp xCoord={xCoord} yCoord={yCoord} />
+          popUpVisible
+          && (
+          <TargetSelectorPopUp
+            xCoord={xCoord}
+            yCoord={yCoord}
+            characters={map.characters}
+          />
+          )
         }
       </div>
     </div>
