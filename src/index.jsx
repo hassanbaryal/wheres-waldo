e5c4d850cf1e5ff225af0fd7898856d9b608ee45
hassanbaryal <h.baryal2@gmail.com>
2023-01-13 19:50:11 -0500

    Replaced App with RouteSwitch

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,11 @@ import './assets/styles/index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import RouteSwitch from './components/Routes/RouteSwitch';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouteSwitch />
   </React.StrictMode>,
 );
