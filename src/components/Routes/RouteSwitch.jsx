6235ba73e6cac074e88fe77cbbf0b19189bce089
hassanbaryal <h.baryal2@gmail.com>
2023-01-13 19:49:49 -0500

    Added route to map one

--- src/components/Routes/RouteSwitch.jsx
+++ src/components/Routes/RouteSwitch.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import Home from '../Home/Home';
 import LevelSelect from '../LevelSelect/LevelSelect';
+import MapOne from '../Maps/MapOne/MapOne';
 
 function RouteSwitch() {
   return (
@@ -9,6 +10,7 @@ function RouteSwitch() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/levelselect" element={<LevelSelect />} />
+        <Route path="/mapone" element={<MapOne />} />
       </Routes>
     </BrowserRouter>
   );
